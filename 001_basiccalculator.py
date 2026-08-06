0650c6265d3debe7e5a8a4c9066aa1afa3b6e800
priyanshu-bio <prgupta104972@gmail.com>
2026-08-06 22:58:38 +0530

    basic calculator script

---
+++ 001_basiccalculator.py
@@ -0,0 +1,12 @@
+a = (input('write 1st no. :'))
+b = (input('write 2nd no. :'))
+c = input('choose oppertor [/*-+]  :')
+if c == '/' :
+    print(a / b)
+
+elif c == '*' :
+    print(a * b)
+elif c == '-' :
+    print(a - b)
+elif c == '+' :
+    print(a + b)
